112672648652cd3c34b7d2e66de250e84a4b090c
leonardb <ddodonova891@gmail.com>
2024-02-07 18:21:01 +0300

    2 commit

--- src/s21_strlen.c
+++ src/s21_strlen.c
@@ -1,3 +1,16 @@
-//
-// Created by Veigar on 07.02.2024.
-//
+//#include <stdio.h>
+
+size_t s21_strlen(const char *str) {
+    size_t length = 0;
+    while (*str++) {
+        length++;
+    }
+    return length;
+}
+
+//int main() {
+//    const char *str = "0123456789";
+//    size_t length = s21_strlen(str);
+//    printf("Length of the string: %zu\n", length);
+//    return 0;
+//}
